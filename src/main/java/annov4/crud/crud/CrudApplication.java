441758aa98f8dc90bd5f9256df487ad352dd5363
annov4 <137298843+annov4@users.noreply.github.com>
2024-08-08 00:32:41 +0300

    deploy112

--- src/main/java/annov4/crud/crud/CrudApplication.java
+++ src/main/java/annov4/crud/crud/CrudApplication.java
@@ -1,9 +1,7 @@
 package annov4.crud.crud;
 
-import annov4.crud.crud.config.WeatherConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
 public class CrudApplication {
--- src/main/java/annov4/crud/crud/config/WeatherConfig.java
+++ src/main/java/annov4/crud/crud/config/WeatherConfig.java
@@ -3,11 +3,13 @@ package annov4.crud.crud.config;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 
 @Getter
 @Setter
 @Configuration
+@EnableConfigurationProperties
 @ConfigurationProperties(prefix = "weather")
 public class WeatherConfig {
     private String apiKey;
