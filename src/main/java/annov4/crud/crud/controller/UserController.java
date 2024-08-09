4b9c82402c1885424291f1318eb067567d926ea4
annov4 <137298843+annov4@users.noreply.github.com>
2024-08-09 04:03:48 +0300

    deploy164

--- src/main/java/annov4/crud/crud/controller/UserController.java
+++ src/main/java/annov4/crud/crud/controller/UserController.java
@@ -4,6 +4,8 @@ import annov4.crud.crud.model.User;
 import annov4.crud.crud.service.UserService;
 import annov4.crud.crud.service.WeatherService;
 import lombok.AllArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,7 +17,6 @@ import java.security.Principal;
 import java.util.HashMap;
 import java.util.Map;
 
-
 @RestController
 @AllArgsConstructor
 @RequestMapping("/api/user")
@@ -23,15 +24,26 @@ public class UserController {
 
     private final UserService userService;
     private final WeatherService weatherService;
+    private static final Logger logger = LoggerFactory.getLogger(UserController.class);
 
     @GetMapping
-    public ResponseEntity getCurrentUser(Principal principal) throws IOException {
-        User user = userService.findByName(principal.getName());
+    public ResponseEntity<Map<String, Object>> getCurrentUser(Principal principal) throws IOException {
+        String username = principal.getName();
+
+        User user = userService.findByName(username);
+
         WeatherService.Coordinates coordinates = weatherService.getCoordinates(user.getAddress());
+        logger.info("info", coordinates.getLatitude(), coordinates.getLongitude());
+
+        logger.info("info", coordinates.getLatitude(), coordinates.getLongitude());
         WeatherService.WeatherInfo weatherInfo = weatherService.getWeatherInfo(coordinates.getLatitude(), coordinates.getLongitude());
+        logger.info("info", weatherInfo.getCondition());
+
         Map<String, Object> response = new HashMap<>();
         response.put("user", user);
         response.put("weatherCondition", weatherInfo.getCondition());
+
         return new ResponseEntity<>(response, HttpStatus.OK);
     }
 }
+
