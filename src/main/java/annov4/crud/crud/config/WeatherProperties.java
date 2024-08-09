e68fe4bfdafec9e0be36431396a71e902a8ae537
annov4 <137298843+annov4@users.noreply.github.com>
2024-08-09 03:45:28 +0300

    deploy163

--- src/main/java/annov4/crud/crud/config/WeatherProperties.java
+++ src/main/java/annov4/crud/crud/config/WeatherProperties.java
@@ -10,7 +10,7 @@ import org.springframework.stereotype.Component;
 @Component
 @ConfigurationProperties(prefix = "weather")
 public class WeatherProperties {
-    private String apiKey = "cb7093256c8ed475de23fb01148b5f690f3206a7";
-    private String secretKey = "47ed84c5594a87a7ba53a7edb3bff33022d66516";
-    private String accessKey = "3cf5af7e-da4a-403d-8864-b452374ec1bc";
+    private String apiKey;
+    private String secretKey;
+    private String accessKey;
 }
